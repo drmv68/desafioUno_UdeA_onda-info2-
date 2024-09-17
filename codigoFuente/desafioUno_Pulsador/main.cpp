7efd40ef80a7b27672b32699cde8b19d8aac7eb7
drmv68 <darimosva7@gmail.com>
2024-09-16 22:23:34 -0500

    Se junto codigo de compañera, ahora tiene lectura de amplitud y frecuencia, mas identificador de señal

--- codigoFuente/desafioUno_Pulsador/main.cpp
+++ codigoFuente/desafioUno_Pulsador/main.cpp
@@ -3,20 +3,39 @@
 const int pinAnalogico = A0;
 const unsigned long intervaloCaptura = 2;
 const unsigned long duracionCaptura = 2000;
+const int numSamples = 500;
+int samples[numSamples];
+
+float calcularAmplitud(int pinAnalogico, unsigned long duracionCaptura, int intervaloCaptura);
+float calcularFrecuencia(int pinAnalogico, unsigned long duracionCaptura, int intervaloCaptura);
+String identificarSenal(int datos[], int longitud);
+int contarPicos(int datos[], int longitud);
+int contarCruces(int datos[], int longitud);
+int contarPico(int datos[], int longitud);
 
 void setup() {
     Serial.begin(9600);
+    delay(2000);
 }
 
 void loop() {
-
     float amplitud = calcularAmplitud(pinAnalogico, duracionCaptura, intervaloCaptura);
     float frecuencia = calcularFrecuencia(pinAnalogico, duracionCaptura, intervaloCaptura);
+
     Serial.print("Amplitud: ");
     Serial.print(amplitud);
     Serial.print(" V, Frecuencia: ");
     Serial.print(frecuencia);
     Serial.println(" Hz");
+
+    for (int i = 0; i < numSamples; i++) {
+        samples[i] = analogRead(pinAnalogico);
+    }
+
+    String tipoSenal = identificarSenal(samples, numSamples);
+    Serial.println("Tipo de señal: " + tipoSenal);
+
+    delay(5000);
 }
 
 float calcularAmplitud(int pinAnalogico, unsigned long duracionCaptura, int intervaloCaptura) {
@@ -62,4 +81,60 @@ float calcularFrecuencia(int pinAnalogico, unsigned long duracionCaptura, int in
     } else {
         return 0.0;
     }
+}
+
+String identificarSenal(int datos[], int longitud) {
+    int picos = contarPicos(datos, longitud);
+    int cruces = contarCruces(datos, longitud);
+    int pico = contarPico(datos, longitud);
+
+    if (pico > 5) {
+        return "Señal desconocida";
+    }
+
+    if (picos >= cruces) {
+        return "Señal Triangular";
+    }
+
+    if (picos == 0) {
+        return "Señal Cuadrada";
+    }
+
+    if (cruces > picos) {
+        return "Señal Senoidal";
+    }
+}
+
+int contarPicos(int datos[], int longitud) {
+    int picos = 0;
+    for (int i = 1; i < longitud - 1; i++) {
+        if (datos[i] > datos[i - 1] && datos[i] > datos[i + 1]) {
+            picos++;
+        }
+    }
+    return picos;
+}
+
+int contarCruces(int datos[], int longitud) {
+    int cruces = 0;
+    for (int i = 1; i < longitud; i++) {
+        if ((datos[i - 1] < 480 && datos[i] >= 480) || (datos[i - 1] >= 480 && datos[i] < 480)) {
+            cruces++;
+        }
+    }
+    return cruces;
+}
+
+int contarPico(int datos[], int longitud) {
+    int picos = 0;
+    int aux = 0;
+    for (int i = 1; i < longitud - 1; i++) {
+        if (datos[i] > datos[i - 1] && datos[i] > datos[i + 1]) {
+            if (datos[i] > aux) {
+                picos++;
+                aux = datos[i];
+            }
+        }
+    }
+    return picos;
 }
